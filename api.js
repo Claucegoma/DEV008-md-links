93229e8587804f39972e22d597877d1aa0389342
claucegoma <arzagitana@ciencias.unam.mx>
2023-08-23 17:34:59 -0600

    api ended

---
+++ api.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const fs = require('fs');
+const chalk= require('chalk');
+
+//se valida si es una ruta existente//
+const filePath = 'C:\\Users\\ceci_\\OneDrive\\Escritorio\\MDLINKS Dev008\\DEV008-md-links\\filesExamples\\withLinks.md'; 
+const isPathValid = (filePath) => {
+   if (fs.existsSync(filePath)) {
+     return true;
+   }else {
+   return false;
+ }
+};
+console.log(chalk.bold.blue(isPathValid(filePath)));
+console.log(chalk.blue('Great! The path exist'));
+
+//se valida si la ruta es relativa o absoluta.
+//se resuelve la Ruta como absoluta (se convierte ruta relativa a absoluta)
+function convertToAbsolute(filePath) {
+   if (path.isAbsolute(filePath)) {
+      return filePath;
+      
+   } else {
+      const absolutePath = (path.join(process.cwd(), filePath));
+      return absolutePath;
+   }
+};
+console.log(chalk.bold.magenta(convertToAbsolute(filePath)));
+console.log(chalk.magenta('Is a absolute path'));
+
+//fs.statSync dice si es archivo o no
+function isFile(filePath) {
+   return fs.statSync(filePath).isFile();
+};
+console.log(chalk.bold.red(isFile(filePath)));
+console.log(chalk.red('Perfect! it is a file'));
+
+ //validar si es un archivo md
+ const fileMd = (filePath) => {
+    return (path.extname(filePath) === ".md");
+ };
+console.log(fileMd(filePath));
+
+
+//Leer archivo (comprobar si tiene links)
+const readFile = (filePath) => new Promise((resolve, reject) => {
+   fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+         reject(error);
+      } else {
+         resolve(data);
+      }
+   });
+});
+//ver en consola
+readFile(filePath)
+   .then(data => {
+      console.log(chalk.inverse.white('Contenido del archivo:'), data); // Aquí imprimes el contenido leído
+   })
+   .catch(error => {
+      console.error('Error:', error);
+   });
+
+//obtener links
+const getLinks = (filePath) => new Promise((resolve, reject) => {
+   const newLinksMd = [];
+   readFile(filePath).then((data) => {
+      const regularExpression = /\[(.+?)\]\((https?:\/\/[^\s)]+)\)/g;
+      let match = regularExpression.exec(data);
+      //si el archivo contiene Links debe retornar un array de links
+      while (match != null) {
+         newLinksMd.push({
+            href: match[2],
+            text: match[1],
+            file: filePath,
+         });
+         match = regularExpression.exec(data);
+      }
+      resolve(newLinksMd);
+   })
+      .catch((error) => reject(error));
+});
+
+//ver en consola
+// Llamada a la función getLinks
+getLinks(filePath)
+   .then(links => {
+      // Imprimir los resultados en la consola
+      if (links.length > 0) {
+         console.log(chalk.inverse.yellowBright('Enlaces encontrados:'));
+         links.forEach(link => {
+            console.log('Texto:', link.text);
+            console.log('URL:', link.href);
+            console.log('Archivo:', link.file);
+            console.log('---');
+            console.log(chalk.inverse.green('Links'))
+         });
+      } else {
+         console.log('No se encontraron enlaces en el archivo.');
+      }
+   })
+   .catch(error => {
+      console.error('Error:', error);
+   });
+
+
+
+module.exports = {
+   convertToAbsolute,
+   fileMd,
+   readFile,
+   getLinks,
+}
--- index.js
+++ index.js
@@ -1,21 +1,49 @@
+const {convertToAbsolute, fileMd, readFile, getLinks} = require('./api.js');
+const { getStatus } = require('./libAxio.js');
+
+//asincronía (librería nativa FS (FileSystem))//
 const fs =require('fs');
 
-const mdLinks = (path, options) => {
-return new Promise((resolve, reject)=>{
-//identifica si la ruta existe//
-if(fs.existsSync(path)){
-//ruta absoluta?//
-//Probar si esa ruta es archivo o directorio//
-//si es un  directorio que s¿filtre los archivos.md//
-}
-else{
-//Si no existe la ruta, rechaza la promesa//
-reject('La ruta no existe');
-}
+//Funcion mdLinks
+const mdLinks = (filePath, options = { validate: false }) => new Promise((resolve, reject) => {
+  const shouldValidate = options.validate;
+  //se convierte de relativa a absluta
+  const absolutePath = convertToAbsolute(filePath);
+  //  pathExist
+  const pathExist = fs.existsSync(absolutePath);
+  if (!pathExist) {
+    return reject(new Error('La ruta no existe ' + absolutePath))
+  }
 
+  // check MDfile
+  const isMdFile = fileMd(absolutePath);
+  if (!isMdFile) {
+    return reject(new Error('El archivo no es Markdown'))
+  }
+  getLinks(absolutePath)
+    .then((links) => {
+      if (!shouldValidate) {
+        return resolve(links)
+      }
+
+      getStatus(links).then((respuesta) => {
+        return resolve(respuesta)
+      })
+    })
+    .catch((error) => {
+      reject(error)
+    }) 
 })
+
+// Llamada a la función mdLinks con un archivo y opciones
+mdLinks('./filesExamples/with-links.md', { validate: true }).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.error(error);
+});
+
+module.exports ={
+  mdLinks
 }
 
-module.exports = () => {
-  mdLinks
-};
+
